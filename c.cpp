22e91e419575e1c008be88dd4d7f1c4d1513797f
adityakumar-14 <hellx1405@gmail.com>
2024-09-04 10:36:13 +0530

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// Aditya Kumar //
+// PRN - 23070123009 //
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
+/*
+Output
+Hello, World!
+*/c
